811b72443e0da80da2ea4572e91919857d354ca4
DarewinWang <1991wangdong@gmail.com>
2018-09-09 20:01:05 +0800

    删除hanlp

--- src/main/java/com/darwin/common/nlp/Segmentor.java
+++ src/main/java/com/darwin/common/nlp/Segmentor.java
@@ -6,18 +6,11 @@ import java.util.List;
 import org.ansj.domain.Term;
 import org.ansj.splitWord.analysis.ToAnalysis;
 
-import com.hankcs.hanlp.HanLP;
-import com.mysql.fabric.xmlrpc.base.Array;
-
 public class Segmentor {
 	public static void main(String[] args) {
 		System.out.println(ToAnalysis.parse("我爱北京天安门"));
 		List<Term> terms = ToAnalysis.parse("我爱北京天安门").getTerms();
 		System.out.println(terms);
-		System.out.println(HanLP.segment("我爱北京天安门"));
-		String document = "Portable版的基本功能指的是，除CRF分词、依存句法分析外的全部功能。如果用户有自定义的需求， 可以使用hanlp.properties进行配置。"
-				+ "Portable版依然可以从hanlp.properties中获取配置信息， 这使得用户可以挂载自定义词典、挂载CRF模型、使用外部词典覆盖内置词典等等。";
-		System.out.println(HanLP.extractKeyword(document, 5));
 	}
 	
 	public static List<String> Seg2Strings(String content){
